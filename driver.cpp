48d4530a0cb53eb6d80a295d7f2ce3387a33c261
Jessy Loy <jloy@stu.ctcd.edu>
2017-04-19 17:36:35 -0500

    Coding Bat String -2 Whaddup

--- driver.cpp
+++ driver.cpp
@@ -5,20 +5,30 @@
 using namespace std;
 
 string helloName(string name);
-
+string makeAbba(string a, string b);
 int main()
 {
-    assert(helloName("Bob") == "Hello Bob!");
-    assert(helloName("Alice") == "Hello Alice!");
-    assert(helloName("X") == "Hello X!");
-    assert(helloName("Joe") == "Hello Joe!");
-    assert(helloName("Cindy") == "Hello Cindy!");
+    //assert(helloName("Bob") == "Hello Bob!");
+    //assert(helloName("Alice") == "Hello Alice!");
+    //assert(helloName("X") == "Hello X!");
+    //assert(helloName("Joe") == "Hello Joe!");
+    //assert(helloName("Cindy");
     
 
+    assert(makeAbba("Hi" , "Bye") == "HiByeByeHi" );
+    assert(makeAbba("Yo" , "Alice") == "YoAliceAliceYo" );
+    assert(makeAbba("What" , "Up") == "WhatUpUpWhat" );
+    assert(makeAbba("Alt" , "Tab") == "AltTabTabAlt" );
+    assert(makeAbba("Git" , "Hub") == "GitHubHubGit" );
     return 0;
 }
 
 string helloName(string name)
 {
     return "Hello " + name + "!";
+}
+
+string makeAbba(string a, string b)
+{
+    return a+b+b+a;
 }
